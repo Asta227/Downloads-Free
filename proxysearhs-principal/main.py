249295f36f3421f8736c0b4801aa9c2285259f70
Asta227 <103799544+Asta227@users.noreply.github.com>
2022-04-16 00:43:07 -0400

    Update and rename proxysearhs-main/main.py to proxysearhs-principal/main.py

--- proxysearhs-principal/main.py
+++ proxysearhs-principal/main.py
@@ -238,7 +238,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'FriendXfriendss'
+        tl_admin_user = 'JARG090922006'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -618,9 +618,9 @@ def onmessage(update,bot:ObigramClient):
             #    bot_token = os.environ.get('bot_token')
             #    
                 # set in debug
-            #    api_id = 7386053
-            #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+            #    api_id = 15076356
+            #    api_hash = 'c55b19ac2a621c243965a61329acf7d5'
+            #    bot_token = '5272615289:AAH-Kyat-zz3LEyMxssg5k361mKtHFYkSEI'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -636,7 +636,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5127607436:AAFnvYgq6X7ZySp9u_m1qKnPB1sTC7X57ro'
+    bot_token = '5272615289:AAH-Kyat-zz3LEyMxssg5k361mKtHFYkSEI'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
